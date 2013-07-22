29a4979dd4803582906dce49b33fc3469a59d88e
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-07-22 15:38:48 +0300

    BAP-1217: Create basic post actions  - added unit test for post action assembles  - added unit tests for list and configurable post actions  - added tests for bundle and DI classes  - small CS fixes

--- Tests/Unit/DependencyInjection/OroTranslationExtensionTest.php
+++ Tests/Unit/DependencyInjection/OroTranslationExtensionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\TranslationBundle\DependencyInjection;
+namespace Oro\Bundle\TranslationBundle\Tests\Unit\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Definition;
 
